20f211c97d776b415c917a4b7272db510fa13ddc
Brayan-nuh <ochiengbrayan55@gmail.com>
2024-11-12 08:09:15 +0300

    contacts

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,11 +1,34 @@
 import React from "react";
+import './Contacts.css';
 function Contacts(){
     return(
-    <div>
-        <h2>contact us</h2>
-        <p>we would like to hear your feedback!</p>
-
-    </div>
+        <div className="contact">
+        <h1>Contact Us</h1>
+        <p>We'd love to hear from you! Whether you have questions, feedback, or just want to say hello, feel free to reach out.</p>
+  
+        <div className="contact-form">
+          <form>
+            <label htmlFor="name">Name</label>
+            <input type="text" id="name" placeholder="Your Name" required />
+  
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" placeholder="Your Email" required />
+  
+            <label htmlFor="message">Message</label>
+            <textarea id="message" rows="4" placeholder="Your Message" required></textarea>
+  
+            <button type="submit">Send Message</button>
+          </form>
+        </div>
+  
+        <div className="social-media">
+          <p>Follow us on social media:</p>
+          <a href="#"target="_blank" className="social-link">Facebook</a>
+          <a href="#" target="_blank"className="social-link">Instagram</a>
+          <a href="#"target="_blank" className="social-link">Twitter</a>
+          <a href="#" target="_blank" className="social-link">Whatsapp</a>
+        </div>
+      </div>
     );
 }
 
