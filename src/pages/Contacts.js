408ca557b4d7ecd62f408d8ee2198eced7bcb638
Brayan-nuh <ochiengbrayan55@gmail.com>
2024-11-13 14:58:41 +0300

    mongo db

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,21 +1,56 @@
-import React from "react";
+import React ,{useState} from "react";
 import './Contacts.css';
 function Contacts(){
+  const [ formData,setFormData]=useState({
+    name:'',
+    email:'',
+    message:''
+
+  });
+
+  const handleChange=(e)=>{
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit=async (e)=> {
+    e.preventDefault();
+    try {
+      const response = await fetch('http://localhost:5000/api/contact', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      if (response.ok) {
+        alert('Message sent successfully!');
+        setFormData({ name: '', email: '', message: '' });
+      } else {
+        alert('Failed to send message.');
+      }
+    } catch (error) {
+      console.error('Error sending message:', error);
+      alert('There was an error. Please try again.');
+    }
+  };
     return(
         <div className="contact">
         <h1>Contact Us</h1>
         <p>We'd love to hear from you! Whether you have questions, feedback, or just want to say hello, feel free to reach out.</p>
   
         <div className="contact-form">
-          <form action="contact.php" method="POST">
+          <form  onSubmit={handleSubmit}>
             <label htmlFor="name">Name</label>
-            <input type="text" id="name" placeholder="Your Name" required />
+            <input type="text" id="name" value={formData.name} onChange={handleChange} required />
   
             <label htmlFor="email">Email</label>
-            <input type="email" id="email" placeholder="Your Email" required />
+            <input type="email" id="email"  value={formData.email} onChange={handleChange} required />
   
             <label htmlFor="message">Message</label>
-            <textarea id="message" rows="4" placeholder="Your Message" required></textarea>
+            <textarea id="message" rows="4" value={formData.message} onChange={handleChange} required></textarea>
   
             <button type="submit">Send Message</button>
           </form>
@@ -23,7 +58,7 @@ function Contacts(){
   
         <div className="social-media">
           <p>Follow us on social media:</p>
-          <a href="https://mail.google.com/ochiengbrayan55@gmail.com"target="_blank" className="social-link">Email us</a>
+          <a href="mailto:ochiengbrayan55@gmail.com"target="_blank" className="social-link">Email us</a>
           <a href="https://www.instagram.com/briannoah82" target="_blank"className="social-link">Instagram</a>
           <a href="https://www.twitter.com/@BrayanOchi73795"target="_blank" className="social-link">Twitter</a>
           <a href="https://wa.me/0701867245" target="_blank" className="social-link">Whatsapp</a>
